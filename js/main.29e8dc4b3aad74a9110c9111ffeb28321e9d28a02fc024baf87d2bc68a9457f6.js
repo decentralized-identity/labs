6d696eb8257a34115f8fae4c439051c284f9089d
andorsk <andorsk@users.noreply.github.com>
2025-01-24 22:22:12 +0000

    deploy: 180f79ceb83ebae9d8f1901e6312f2da2cb2cd68

---
+++ js/main.29e8dc4b3aad74a9110c9111ffeb28321e9d28a02fc024baf87d2bc68a9457f6.js
@@ -0,0 +1,1 @@
+(()=>{document.addEventListener("DOMContentLoaded",()=>{let e=document.querySelector(".hamburger"),t=document.querySelector(".nav-links");e.addEventListener("click",()=>{t.classList.toggle("active")})});})();
